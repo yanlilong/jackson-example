d459677c1c3e243249809ee45c93a923fabdaf42
yanlilong <yan.w.li@outlook.com>
2021-01-04 22:38:37 +0100

    create new project

---
+++ src/test/java/com/yanli/jackson/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanli.jackson.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
